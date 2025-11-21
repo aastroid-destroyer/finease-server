117f0145851e48448a15a00d16e2a628e46db13f
aastroid-destroyer <abraraqib33@gmail.com>
2025-11-21 17:00:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7i9hgzt.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('finease_db');
+    const trancollections = db.collection('transactions');
+
+    // get
+    app.get('/transactions', verifyToken, async (req, res) => {
+      const { email } = req.query;
+
+      let filter = {};
+      if (email) { filter.email = email; }
+
+      const result = await trancollections.find(filter).sort({ date: -1 }).toArray();
+      res.send(result);
+    });
+
+
+    // DB Connection check
+    await client.db('admin').command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
